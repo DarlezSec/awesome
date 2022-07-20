8aee63199716f3c16898652cca858fd97fe893bc
r3pt0un <102554737+DarlezSec@users.noreply.github.com>
2022-07-20 12:37:46 -0500

    Add files via upload

---
+++ cfg/awesome/themes/Morning/theme.lua
@@ -0,0 +1,37 @@
+-- Morning
+------------------
+
+local theme = {}
+
+theme.font          = 'JetBrains Mono 10'
+
+theme.bg_normal     = "#545454"
+theme.bg_focus      = "#545454"
+theme.bg_urgent     = "#545454"
+theme.bg_minimize   = "#545454"
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = "#545454"
+theme.fg_focus      = "#545454"
+theme.fg_urgent     = "#545454"
+theme.fg_minimize   = "#545454"
+
+theme.useless_gap   = 7
+theme.border_width  = 0
+theme.border_normal = "#545454"
+theme.border_focus  = "#545454"
+theme.border_marked = "#545454"
+
+-- Hotkeys popup
+theme.hotkeys_bg = '#000000'
+theme.hotkeys_fg = '#ffffff'
+theme.hotkeys_opacity = '0.9'
+theme.hotkeys_border_width = 2
+theme.hotkeys_border_color = '#545454'
+theme.hotkeys_modifiers_fg = '#88664A'
+theme.hotkeys_label_fg = '#ffffff'
+theme.hotkeys_font = 'JetBrains Mono 10'
+theme.hotkeys_description_font = 'Source Code Pro 10'
+theme.hotkeys_group_margin = 20
+
+return theme
